3b9626d3bcd1fd425c165b04e080904471e517ac
ozimko93 <ozimko93@gmail.com>
2024-07-25 22:12:05 +0300

    added react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from 'react-google-charts';
 
+export const data = [
+  ['Жанр', 'Количество'],
+  ['Исторический', 9],
+  ['Комедия', 6],
+  ['Приключения', 24],
+  ['Боевик', 35],
+  ['Романтический', 26],
+];
+
+export const options = {
+  title: 'Репертуар кинотеатра по жанрам за 2024 год',
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Chart-container">
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={'100%'}
+        height={'600px'}
+      />
     </div>
   );
 }
